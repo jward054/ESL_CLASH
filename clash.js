125419845d294dcc99b0aa33b67e1311dd0f6df0
eosaben <eosaben@cs.odu.edu>
2014-12-28 10:50:46 -0500

    Initial Commit
    Base Directory Structure and place holder files

---
+++ clash.js
@@ -0,0 +1,21 @@
+/*
+* clash.js
+* Root namespace module
+*/
+/*jslint browser : true, continue : true,
+devel : true, indent : 2, maxerr : 50,
+newcap : true, nomen : true, plusplus : true,
+regexp : true, sloppy : true, vars : false,
+white : true
+*/
+/*global $, clash */
+var clash = (function () {
+var initModule = function ( $container ) {
+$container.html(
+	'<h1 style="display:inline-block; margin:25px;">'
+	+ 'hello world!'
+	+ '</h1>'
+);
+};
+return { initModule: initModule };
+}());
---
+++ colrs/colrs.js
@@ -0,0 +1,17 @@
+/*
+* colrs.js
+* Root namespace module
+*/
+/*jslint browser : true, continue : true,
+devel : true, indent : 2, maxerr : 50,
+newcap : true, nomen : true, plusplus : true,
+regexp : true, sloppy : true, vars : false,
+white : true
+*/
+/*global $, clash */
+
+/**
+*
+*	TODO
+*
+**/
